a22573f6fef99418fe5137e2cb75e03d1d0dc2c4
Baptiste D <baltazar.14130@gmail.com>
2024-02-19 16:20:42 +0100

    remove swagger redirect

--- src/main/java/org/alica/api/config/WebConfig.java
+++ src/main/java/org/alica/api/config/WebConfig.java
@@ -2,7 +2,6 @@ package org.alica.api.config;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
@@ -13,9 +12,5 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addRedirectViewController("/", "/swagger-ui.html");
-    }
+    
 }
